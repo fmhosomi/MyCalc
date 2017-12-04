69151c6f2401d5bc228727a2c5443eb55440807a
oul <tianmenglou@gmail.com>
2017-12-04 16:51:39 +0900

    Add Buttons

--- MyCalc/ViewController.swift
+++ MyCalc/ViewController.swift
@@ -22,6 +22,8 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    // MARK: - Util
+
     func scrollToBottom() {
         calcField.selectedRange = NSRange(location: calcField.text.characters.count, length: 0)
         calcField.isScrollEnabled = true
@@ -39,11 +41,17 @@ class ViewController: UIViewController {
     func addTextWithNewLine(_ str: String) {
         addText("\n\(str)\n")
     }
+
+    // MARK: - Actions
     
     @IBAction func tap0Button(_ sender: Any) {
         addText("0")
     }
-    
+
+    @IBAction func tap00Button(_ sender: Any) {
+        addText("00")
+    }
+
     @IBAction func tap1Button(_ sender: Any) {
         addText("1")
     }
@@ -55,11 +63,65 @@ class ViewController: UIViewController {
     @IBAction func tap3Button(_ sender: Any) {
         addText("3")
     }
-    
+
+    @IBAction func tap4Button(_ sender: Any) {
+        addText("4")
+    }
+
+    @IBAction func tap5Button(_ sender: Any) {
+        addText("5")
+    }
+
+    @IBAction func tap6Button(_ sender: Any) {
+        addText("6")
+    }
+
+    @IBAction func tap7Button(_ sender: Any) {
+        addText("7")
+    }
+
+    @IBAction func tap8Button(_ sender: Any) {
+        addText("8")
+    }
+
+    @IBAction func tap9Button(_ sender: Any) {
+        addText("9")
+    }
+
+    @IBAction func tapDotButton(_ sender: Any) {
+        addText(".")
+    }
+
     @IBAction func tapPlusButton(_ sender: Any) {
         addText("+")
     }
-    
+
+    @IBAction func tapMinusButton(_ sender: Any) {
+        addText("-")
+    }
+
+    @IBAction func tapTimesButton(_ sender: Any) {
+        addText("*")
+    }
+
+    @IBAction func tapDivideButton(_ sender: Any) {
+        addText("/")
+    }
+
+    @IBAction func tapPercentButton(_ sender: Any) {
+//        addText("")
+    }
+
+    @IBAction func tapAllClearButton(_ sender: Any) {
+        var equations = calcField.text.components(separatedBy: "\n")
+        equations.removeLast()
+        calcField.text = equations.joined(separator: "\n") + "\n"
+    }
+
+    @IBAction func tapDeleteButton(_ sender: Any) {
+        calcField.text.remove(at: calcField.text.index(before: calcField.text.endIndex))
+    }
+
     @IBAction func tapEqualButton(_ sender: Any) {
         let text: String
         defer { addTextWithNewLine(text) }
